22f40f1f16a27046873a023428874d49bd4ff29a
Regis Araujo Melo <regis@r3tecnologia.net>
2024-03-27 21:30:55 -0300

    package

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "rdcomponents",
-            targets: ["rdcomponents"]),
+            name: "RDComponents",
+            targets: ["RDComponents"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "rdcomponents"),
+            name: "RDComponents"),
         .testTarget(
-            name: "rdcomponentsTests",
-            dependencies: ["rdcomponents"]),
+            name: "RDComponentsTests",
+            dependencies: ["RDComponents"]),
     ]
 )
